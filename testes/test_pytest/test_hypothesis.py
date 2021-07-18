332bac840487363ff38dda25ed912ff0436567ee
Samuel <samuel.chenatti@gmail.com>
2021-07-18 18:10:40 -0300

    Adds RegEx example to hypothesis examples

--- testes/test_pytest/test_hypothesis.py
+++ testes/test_pytest/test_hypothesis.py
@@ -1,7 +1,21 @@
 from hypothesis import given, strategies as st
+import pytest
+
+CPF_REGEX = (
+    r'([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|'
+    r'([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})'
+)
 
 
+@pytest.mark.skip('Gera muito lixo no stdout')
 @given(st.integers(min_value=0))
 def test_float(integer: int):
     print('Integer:', integer)
     assert isinstance(integer, int)
+
+
+@pytest.mark.skip('Gera muito lixo no stdout')
+@given(st.from_regex(CPF_REGEX))
+def test_cpfs(cpf: str):
+    print('CPF:', cpf)
+    assert isinstance(cpf, str)
